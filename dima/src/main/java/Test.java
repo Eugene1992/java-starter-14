63c3e015d9f2c4b291a4a76fcc18aaad988e9439
Kasad0r <dimon.pokemon99@gmail.com>
2016-10-05 08:45:39 +0300

    How does it works??

--- dima/src/main/java/Test.java
+++ dima/src/main/java/Test.java
@@ -2,35 +2,4 @@
  * Created by evgeniy on 04/10/16.
  */
 public class Test {
-    /**
-     * Created by Student on 10/4/2016.
-     */
-        public static void main(String args[]){
-
-            // byte b = 126;
-            // short sh = 5342;
-            // int i = 584371902;
-            // long l = 532785943234L;
-            // byte a = 13+5;
-            // short b = 5435-1362;
-            // int i = 435723/7234;
-            // long l = 1234892314L*1324814235L;
-
-
-            short sh1 = 52, sh2 = 24;
-            byte bt1 = 2, bt2 = 4;
-            byte bt3 = (byte) (bt1 + bt2);
-            byte bt4 = (byte) ((sh1+sh2)/bt3);
-            int i = (sh1/sh2) * (bt1 / bt2);
-
-            int lot=5,kot = 21,bot;
-            bot = kot;
-            kot = lot;
-            kot = bot;
-
-            int zx=2,xc=3;
-
-
-        }
-    }
-
+}

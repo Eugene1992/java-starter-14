5ab2320ed202a1558e392d66659aa95edf317a30
Kasad0r <dimon.pokemon99@gmail.com>
2016-10-07 10:29:06 +0300

    Add Ex8

--- dima/src/main/java/hw_04/Hw4Ex8.java
+++ dima/src/main/java/hw_04/Hw4Ex8.java
@@ -7,6 +7,17 @@ public class Hw4Ex8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int n;
+        int count = 0;
+        System.out.print("Вводите ваше натуральное число - ");
+        Scanner sr = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.print("Число " + n + " делится на - " );
+        for(int i = 1; i <= n; i++){
+            if ((n % i) == 0){
+                System.out.print(i + " ");
+            }
+        }
 
     }
 }

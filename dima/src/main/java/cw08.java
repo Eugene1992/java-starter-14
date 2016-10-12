7f22b7923c6af0e9739f3017dcc4bfb489fd4ad3
Kasad0r <dimon.pokemon99@gmail.com>
2016-10-12 18:07:46 +0300

    cw08

--- dima/src/main/java/cw08.java
+++ dima/src/main/java/cw08.java
@@ -3,13 +3,24 @@
  */
 public class cw08 {
     public static void main(String[] args) {
-        compare("Sample","Text");
+        System.out.println(compare("Halo", "Text"));
+
     }
-    static void compare(String s, String s1){
-        
+
+    static boolean compare(String s, String s1) {
+        char[] array1 = s.toCharArray();
+        char[] array2 = s1.toCharArray();
+        int lngthS = s.length() - 1;
+        int lngthS1 = s1.length() - 1;
+        if (lngthS == lngthS1) {
+            for (int a = 0; a < lngthS; a++) {
+                if (array1[a] == array2[a]) continue;
+                {
+                    return false;
+                }
+            }
         }
 
+        return true;
     }
-
-
 }

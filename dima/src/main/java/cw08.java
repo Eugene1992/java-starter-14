ba753c9f776bcbb814feb34570af393cb4ef594a
Kasad0r <dimon.pokemon99@gmail.com>
2016-10-12 17:46:44 +0300

    Merge branch 'master' of D:\Idea\java-starter-14 with conflicts.

---
+++ dima/src/main/java/cw08.java
@@ -0,0 +1,15 @@
+/**
+ * Created by dimon on 12.10.2016.
+ */
+public class cw08 {
+    public static void main(String[] args) {
+        compare("Sample","Text");
+    }
+    static void compare(String s, String s1){
+        
+        }
+
+    }
+
+
+}
--- dima/src/main/java/hw_07/hw07Ex.java
+++ dima/src/main/java/hw_07/hw07Ex.java
@@ -1,19 +1,23 @@
 package hw_07;
-
+import java.util.Scanner;
 /**
  * Created by dimon on 12.10.2016.
  */
 public class hw07Ex {
+
     public static void main(String[] args) {
-        int myArray[] = {3, 5, 7, 12};
+        int myArray[] = {3, 5, 7, 12, -1};
         for (int i = 0; i < myArray.length; i++) {
             int i2 = i + 1;
             if (i2 >= myArray.length - 1) {
+                i2 = 0;
                 i2 = 0;
             }
             int sum = myArray[i] + myArray[i2];
             System.out.println(sum);
         }
+
+
     }
 }
 
